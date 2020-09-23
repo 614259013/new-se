84a3aef7e5272f9a82da0cde6df192ce470fd275
Nattanai <614259013@webmail.npru.ac.th>
2020-09-23 14:59:04 +0700

    First Project

---
+++ application/views/index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/index.css">
+
+</head>
+<body>
+    <!------22252c---->
+    <img src="../image/bg.jpg">
+    <div class="botton-neon">
+    <a href="formLogin.php">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        LOG IN
+    </a>
+
+    <a href="<?php echo base_url(); ?>index.php/Welcome/signup">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        SIGN UP
+    </a>
+    </div>
+</body>
+</html>
